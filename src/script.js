fd882045b91b46c01d5f069bb720e99406a2e609
Naeem-wq <95334435+Naeem-wq@users.noreply.github.com>
2022-06-08 22:45:00 +0200

    Added styling and Javascript functionality.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,8 @@
-    
+let inputText;
+let tasksArr = [];
+
+function addUser() {
+  inputText = document.getElementById("userInput").value;
+  document.getElementById("display").innerHTML = inputText;
+  tasksArr.push(inputText);
+}
