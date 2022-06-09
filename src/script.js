2faca6096fc1c9d018b7c2899a7fa0c05e661a86
Naeem-wq <95334435+Naeem-wq@users.noreply.github.com>
2022-06-09 22:04:17 +0200

    added local storage

--- src/script.js
+++ src/script.js
@@ -1,18 +1,27 @@
 let inputText;
 let tasksArr = [];
+const todoWebApp = "todo-web-app";
 
 function addUser() {
-  // document.getElementById("display").innerHTML += (' ')
-
   inputText = document.getElementById("userInput").value;
   tasksArr.push(inputText);
 
-  for(let i = 0; i < tasksArr.length; i++){
-     
+  for (let i = 0; i < tasksArr.length; i++) {
     let ourDisplayText = document.createElement("h5");
     let text = document.createTextNode(tasksArr);
     ourDisplayText.appendChild(text);
     document.getElementById("display").appendChild(ourDisplayText);
   }
 
+  addLocalStorage();
 }
+
+/**********************************
+ *        Local Storage
+ *********************************/
+function addLocalStorage() {
+  tasksArr = JSON.stringify(tasksArr);
+  localStorage.setItem(todoWebApp, tasksArr);
+  tasksArr = JSON.parse(localStorage.getItem(todoWebApp));
+}
+// let myStorage = localStorage.setItem("Tasks",tasksArr);
