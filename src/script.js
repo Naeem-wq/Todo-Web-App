4de89efe9c7c968c5bc2664d875aac419f8ac5cf
Naeem-wq <95334435+Naeem-wq@users.noreply.github.com>
2022-06-09 17:22:06 +0200

    Added more Javascript

--- src/script.js
+++ src/script.js
@@ -2,7 +2,17 @@ let inputText;
 let tasksArr = [];
 
 function addUser() {
+  // document.getElementById("display").innerHTML += (' ')
+
   inputText = document.getElementById("userInput").value;
-  document.getElementById("display").innerHTML = inputText;
   tasksArr.push(inputText);
+
+  for(let i = 0; i < tasksArr.length; i++){
+     
+    let ourDisplayText = document.createElement("h5");
+    let text = document.createTextNode(tasksArr);
+    ourDisplayText.appendChild(text);
+    document.getElementById("display").appendChild(ourDisplayText);
+  }
+
 }
